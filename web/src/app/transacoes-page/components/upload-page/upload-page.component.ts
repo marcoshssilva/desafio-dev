e914b16b3da723e6460750e5d950db288cdd8cc1
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-04-10 11:23:27 -0300

    implementado funcao upload de arquivo para transacoes

--- web/src/app/transacoes-page/components/upload-page/upload-page.component.ts
+++ web/src/app/transacoes-page/components/upload-page/upload-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { TransacaoService } from 'src/app/shared/services/transacao.service';
 
 @Component({
   selector: 'app-upload-page',
   templateUrl: './upload-page.component.html',
-  styleUrls: ['./upload-page.component.css']
+  styleUrls: ['./upload-page.component.css'],
 })
 export class UploadPageComponent implements OnInit {
+  file?: File;
 
-  constructor() { }
+  constructor(private transacaoService: TransacaoService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onFileChoose(event: any) {
+    this.file = event.target.files[0];
   }
 
+  uploadFile() {
+    if (this.file) {
+      this.transacaoService
+        .uploadFile(this.file)
+        .subscribe((data) => console.log(data));
+    }
+  }
 }

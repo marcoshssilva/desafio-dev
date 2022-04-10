b8a210d6c445ab21ea6c4e03a2b2fb0089a301ed
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-04-10 09:02:25 -0300

    Teste de visualização implementado.

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LojaModel } from './shared/models/LojaModel';
 import { TransacaoService } from './shared/services/transacao.service';
 
 @Component({
@@ -6,13 +7,23 @@ import { TransacaoService } from './shared/services/transacao.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'desafiodev-frontend';
+  private lojas : LojaModel[] = []
+
   constructor(
     private transacaoService : TransacaoService
   ) {}
 
-  get Lojas$() {
-    return this.transacaoService.getTransacoesResumedByLoja();
+  ngOnInit(): void {
+    this.transacaoService.getTransacoesResumedByLoja().subscribe(data => {
+      this.lojas = data
+    })
   }
+
+  get Lojas() : LojaModel[] {
+    return this.lojas
+  }
+
 }

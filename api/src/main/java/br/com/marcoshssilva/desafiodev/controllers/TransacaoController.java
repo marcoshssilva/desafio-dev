636ae66cddd4258d2a41a8d1809a1e66d45d83c9
Marcos Henrique <76234459+marcoshssilva@users.noreply.github.com>
2022-04-10 20:30:40 -0300

    Implementando mensagens de apoio na documentação

--- api/src/main/java/br/com/marcoshssilva/desafiodev/controllers/TransacaoController.java
+++ api/src/main/java/br/com/marcoshssilva/desafiodev/controllers/TransacaoController.java
@@ -5,6 +5,10 @@ import br.com.marcoshssilva.desafiodev.models.LojaModelDto;
 import br.com.marcoshssilva.desafiodev.services.FileReaderTransacoesUtilService;
 import br.com.marcoshssilva.desafiodev.services.TransacaoService;
 import br.com.marcoshssilva.desafiodev.services.exceptions.ValidationException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +22,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 
+@Api("Transacoes")
 @RestController
 @RequestMapping("/transacoes")
 public class TransacaoController {
@@ -33,18 +38,28 @@ public class TransacaoController {
         this.fileReaderTransacoesUtilService = fileReaderTransacoesUtilService;
     }
 
+    @ApiOperation("Retorna uma lista de transacoes")
+    @ApiResponses(
+            @ApiResponse(code = 200, message = "OK")
+    )
     @GetMapping()
     public ResponseEntity<List<Transacao>> getAllTransacoes() {
         List<Transacao> transacaos = this.transacaoService.getAll();
         return ResponseEntity.ok(transacaos);
     }
 
+    @ApiOperation("Retorna uma lista de lojas com suas correspondentes transacoes e saldo")
     @GetMapping("/lojas")
     public ResponseEntity<List<LojaModelDto>> getAllTransacoesResumedByLoja() {
         List<LojaModelDto> lojas = this.transacaoService.getAllResumedByLoja();
         return ResponseEntity.ok(lojas);
     }
 
+    @ApiOperation(code = 204, value = "Le e salva as transações de um arquivo TXT recebido.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Todas as transações foram lidas e salvas com sucesso."),
+            @ApiResponse(code = 422, message = "Houve erro na validação de algum dado do arquivo.")
+    })
     @PostMapping("/upload")
     public ResponseEntity<?> uploadTransacoesByFile(@RequestParam(name = "file") MultipartFile multipartFile) throws IOException, ValidationException {
 
